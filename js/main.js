1433a9b44bbcf6305589a5d553cb3d8af3a84c39
Tomasz Bielski <ttbielski@gmail.com>
2021-11-02 18:15:02 +0100

    Add changing username functionality.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log('Test');
+const changeBtn = document.querySelector('#change_user')
+const userName = document.querySelector('#username');
+
+if (!localStorage.getItem('username')) {
+    userName.textContent = 'everyone';
+} else {
+    userName.textContent = localStorage.getItem('username');
+}
+
+function setUserName() {
+    let yourName = prompt('Enter your name:');
+    localStorage.setItem('username', yourName);
+    userName.textContent = localStorage.getItem('username');
+}
+
+changeBtn.addEventListener('click', setUserName);
