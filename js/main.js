21018392114d830beffcffd16f1953327ab25f44
Tomasz Bielski <ttbielski@gmail.com>
2021-11-02 18:21:57 +0100

    Change indentation to 2 spaces.

--- js/main.js
+++ js/main.js
@@ -2,15 +2,15 @@ const changeBtn = document.querySelector('#change_user')
 const userName = document.querySelector('#username');
 
 if (!localStorage.getItem('username')) {
-    userName.textContent = 'everyone';
+	userName.textContent = 'everyone';
 } else {
-    userName.textContent = localStorage.getItem('username');
+	userName.textContent = localStorage.getItem('username');
 }
 
 function setUserName() {
-    let yourName = prompt('Enter your name:');
-    localStorage.setItem('username', yourName);
-    userName.textContent = localStorage.getItem('username');
+	let yourName = prompt('Enter your name:');
+	localStorage.setItem('username', yourName);
+	userName.textContent = localStorage.getItem('username');
 }
 
 changeBtn.addEventListener('click', setUserName);
